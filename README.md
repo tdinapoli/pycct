972ad2c4b83197118580bad229c6b44055a27763
tdinapoli <tomas.dina98@gmail.com>
2025-12-16 17:33:53 -0300

    Structure as a package

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# pycct
+
+A python package to control THORLABS cmos spectometers via serial commands.
---
+++ src/pycct/__init__.py
@@ -0,0 +1,4 @@
+from .spectrometer import Spectrometer
+from .pyserial_client import PySerialJetiClient
+
+__all__ = ["Spectrometer", "PySerialJetiClient"]
